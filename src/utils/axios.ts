a34670d215b3b9c7e50cc8fb36ce950f2e8348ba
vitaly-healthera <vitaly@healthera.co.uk>
2021-08-30 13:28:37 +0300

    feat: retry axios request on access token expiration

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
 import { logout, refreshTokensRequest } from "../redux/auth/actions";
 import { store } from "../redux/store";
+
+const REFRESH_TOKEN_URL = "/user/refresh-token";
 
 const instance = axios.create({
   baseURL: "http://localhost:3002",
@@ -13,11 +15,6 @@ instance.interceptors.response.use(
   (err) => {
     const { status } = err.response || {};
 
-    if (status === 401) {
-      err.isTokenExpired = true;
-      store.dispatch(refreshTokensRequest());
-    }
-
     if (status === 403) {
       store.dispatch(logout());
     }
@@ -26,4 +23,54 @@ instance.interceptors.response.use(
   }
 );
 
-export default instance;
+let tokensRefreshed = true;
+
+const axiosProxy = new Proxy(instance, {
+  get: (obj: AxiosInstance, prop: keyof AxiosInstance) => {
+    if (typeof obj[prop] !== "function") return obj[prop];
+
+    return async (...args: any[]) => {
+      try {
+        const response = await (obj[prop] as Function).apply(obj, args);
+
+        if (response.config.url === REFRESH_TOKEN_URL) {
+          tokensRefreshed = true;
+        }
+
+        return response;
+      } catch (error) {
+        if (error.response?.status === 401) {
+          tokensRefreshed = false;
+
+          error.isTokenExpired = true;
+          store.dispatch(refreshTokensRequest());
+
+          return new Promise((resolve, reject) => {
+            const check = async () => {
+              if (tokensRefreshed) {
+                try {
+                  const response = await (obj[prop] as Function).apply(
+                    obj,
+                    args
+                  );
+                  resolve(response);
+                } catch (error) {
+                  reject(error);
+                }
+                return;
+              }
+
+              setTimeout(check, 100);
+            };
+
+            check();
+          });
+        } else {
+          return error;
+        }
+      }
+    };
+  },
+});
+
+export default axiosProxy;
